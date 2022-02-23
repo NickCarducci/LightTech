e96281b26658e371f432979e55486d5965d9c8b3
Nick Carducci <nmcarducci@gmail.com>
2022-02-23 18:40:19 -0500

    Update Dropwire.js

--- src/Dropwire.js
+++ src/Dropwire.js
@@ -3,7 +3,7 @@ import React from "react";
 //import ReactHtmlParser from "react-html-parser";
 //import reactElementToJSXString from "react-element-to-jsx-string";
 //import ReactDOMServer from "react-dom/server";
-import ExecutionEnvironment from "exenv";
+//import ExecutionEnvironment from "exenv";
 
 /*class Forward extends React.Component {
   constructor(props) {
@@ -23,16 +23,16 @@ class Cable extends React.Component {
       cache: null,
       mountsCount: 0,
       cacheStyle: "",
-      framewidth: 200
+      go: true
     };
     this.page = React.createRef();
     this.fwdtwe = React.createRef();
   }
-  componentDidMount() {
+  /*componentDidMount() {
     if (ExecutionEnvironment.canUseDOM) {
       this.setState({ go: true }, () => this.checkIfBetween());
     }
-  }
+  }*/
   componentDidUpdate = (prevProps) => {
     if (this.state.go && this.props.scrolling !== prevProps.scrolling) {
       this.checkIfBetween();
@@ -50,24 +50,57 @@ class Cable extends React.Component {
     clearTimeout(this.setset);
   };
   checkIfBetween = () => {
-    const { cache } = this.state;
-    const { scrollTopAndHeight, scrollTop, girth, timeout } = this.props;
-    var girt = girth ? girth : 500;
+    const { /*frameheight,*/ cache } = this.state;
+    const { /*scrollTopAndHeight,*/ scrollTop, girth, timeout } = this.props;
+    var girt =
+      girth && !isNaN(girth)
+        ? girth + 500
+        : window.innerHeight; /*frameheight
+        ? frameheight
+        : Style &&
+          Style.height &&
+          !isNaN(Style.height)
+        ? Style.height + 500
+        : 500;*/
     var timeou = timeout ? timeout : 1500;
     clearTimeout(this.setset);
     this.setset = setTimeout(() => {
       var page = this.page.current;
       var between =
-        page.offsetTop - scrollTop > Number(`-${girt}`) &&
-        scrollTopAndHeight - page.offsetTop > Number(`-${girt}`);
-
+        //Math.abs(scrollTop + page.offsetTop - window.scrollY) <
+        //girt + window.innerHeight;
+        Math.abs(page.offsetTop - scrollTop) < girt;
+      /*Number(`-${girt}`) &&
+        scrollTopAndHeight - page.offsetTop > Number(`-${girt}`);*/
+      /* Math.abs(
+            scrollTop +
+              page.offsetTop -
+              (window.scrollY +
+                window.innerHeight /
+                  2) /*+ window.innerHeight / 2 - page.offsetTop*
+          )
+          Math.abs(
+          scrollTop +
+            page.offsetTop -
+            (window.scrollY +
+              window.innerHeight /
+                2) /*+ window.innerHeight / 2 - page.offsetTop*
+        ) < girt; //Number(`-${girt}`);*/
+      //console.log(page.offsetTop);
       if (!this.state.mount) {
+        /*console.log(
+          Math.abs(scrollTop + page.offsetTop - window.scrollY),
+          scrollTop,
+          page.offsetTop,
+          window.scrollY ,
+          girt
+        );*/
         //console.log(between, page.offsetTop, scrollTop);
-        this.setState({ mount: between }, () => {});
+        /*between && */ this.setState({ mount: between }, () => {});
       } else {
-        var continuee = this.props.fwd.current;
+        var continuee = this.props.fwd && this.props.fwd.current;
         //between && console.log(between, continuee.outerHTML);
-        //if (!continuee && !cache) return;
+        if (!continuee && !cache) return;
         /*const cacheStyle = JSON.parse(
           (cache ? cache : continuee.outerHTML)
             .split(`style="`)[1]
@@ -77,7 +110,8 @@ class Cable extends React.Component {
         );*/
         //console.log(cacheStyle);
         //console.log(cache, continuee.offsetHeight, continuee.offsetWidth);
-        if (!cache && this.state.loaded) {
+        if (!cache && (this.state.loaded || this.props.img)) {
+          //if (continuee.offsetHeight !== 0)
           this.setState({
             cache: continuee.outerHTML,
             //cacheStyle,
@@ -85,35 +119,47 @@ class Cable extends React.Component {
             framewidth: continuee.offsetWidth
           });
         } else if (!between) {
+          //console.log("!between", continuee.outerHTML);
           /* if (continuee) {
                 
                 const children = [...continuee.children];
                 console.log(children);
                 if (children.length > 0) {
                   var gl = null;
-
                   const foun = children.find(
                     (x) => (gl = x.getContext("webgl"))
                   );
-
                   foun.addEventListener(
                     "webglcontextlost",
                     (e) => console.log(e),
                     false
                   );
-
                   gl.getExtension("WEBGL_lose_context").loseContext();
                 }
               }*/
-          continuee.remove();
-          return (page.innerHTML = "");
+          //continuee.remove();
+          //if (scrollTop !== 0) return;
+          //continuee && continuee.remove();
+
+          if (continuee) {
+            while (continuee.children.length > 0) {
+              continuee.remove(
+                continuee.children[continuee.children.length - 1]
+              );
+            }
+          }
+          //      console.log(girt);
+          //if (Object.keys(page.children).length !== 0 /*page.innerHTML !== ""*/)
+          //return (page.innerHTML = "");
           // this.setState({ mount: false });
-        } else if (page.innerHTML === "") {
+        } /*if (page.innerHTML === "") */ else {
           const children = [...page.children];
           if (
+            //frusterated the second, paniced the first" ca
             cache &&
             (children.length === 0 || !children.find((x) => x === cache))
           ) {
+            console.log("reloading");
             //console.log("replenishing, new scroll", cache);
             return (page.innerHTML = this.state.cache);
           }
@@ -122,10 +168,11 @@ class Cable extends React.Component {
     }, timeou);
   };
   render() {
-    const { mount /*, cacheStyle */ } = this.state;
-    const { src, float, title, img } = this.props;
+    const { mount, stopfail /*, cacheStyle */ } = this.state;
+    const { src, float, title, img, style: Style } = this.props;
     //const limited = limit.find((x) => x === Object.keys(this.props.fwd));
     const onError = (e) => {
+      this.setState({ stopfail: true });
       //this.props.fwd.current.remove();
       this.props.onError(e);
     }; //ternaries remove the node and element; display removes the element, but not the node
@@ -136,30 +183,67 @@ class Cable extends React.Component {
         loaded: true
       });
     };
+    const optionalwidth =
+      !stopfail &&
+      /*(this.state.img || this.state.loaded) && this.state.framewidth
+        ? this.state.framewidth
+        :*/ this
+        .props.style &&
+      Style.width // &&
+        ? //!isNaN(Style.width)
+          Style.width
+        : 200;
+    const optionalheight =
+      !stopfail &&
+      /*this.state.height
+      ? this.state.height
+      :*/ Style &&
+      Style.height &&
+      (!isNaN(Style.height) ||
+        (Style.height.constructor === String &&
+          !isNaN(Number(Style.height.substring(0, Style.height.length - 2)))))
+        ? Style.height
+        : this.props.img
+        ? "min-content"
+        : "100%";
+    //console.log(optionalwidth);
     return (
       <div
         ref={this.page}
         style={{
+          position: "relative",
+          boxShadow: "inset 0px 0px 50px 15px rgb(200,100,120)",
+          //width: this.state.framewidth,
+          ...Style,
+          //overflowX: "auto",
           shapeOutside: "rect()",
           float,
-          height: this.state.frameheight,
-          width: this.state.framewidth,
-          ...this.props.style
+          overflow: "hidden",
+          height: !this.state.loaded ? "" : optionalheight,
+          /*this.state.frameheight
+            ? this.state.frameheight + 10
+            : "max-content",*/
+          width: optionalwidth // "max-content"
+          //maxWidth: "100%"
+          //minWidth: optionalwidth // "max-content"
         }}
       >
-        {!mount || src === "" ? (
-          <span style={{ border: "1px gray solid" }}>{title}</span>
+        {src === "" || (!img && !mount) ? (
+          <span style={{ border: "2px gray solid" }}>{title}</span>
         ) : img ? (
           <img
-            onLoad={onLoad}
+            //onLoad={onLoad}
             onError={onError}
             alt={title}
             style={{
-              shapeOutside: "rect()",
-              float,
-              width: "200px",
-              border: 0,
-              ...this.props.style
+              position: "relative",
+              //width: "100%",
+              border: src === "" ? "2px gray solid" : 0,
+              //...Style,
+              height: Style && !isNaN(Style.width) ? "auto" : optionalheight, //"100%"
+              width: Style && !isNaN(Style.height) ? "auto" : optionalwidth // "100%" // "max-content"
+              //overflowX: "auto",
+              //maxWidth: "100%"
             }}
             ref={this.props.fwd}
             src={src}
@@ -170,11 +254,15 @@ class Cable extends React.Component {
             onError={onError}
             title={title}
             style={{
-              shapeOutside: "rect()",
-              float,
-              width: "200px",
+              position: "relative",
+              //width: "100%",
               border: 0,
-              ...this.props.style
+              //...Style,
+              //height: optionalheight,
+              //width: optionalwidth, // "max-content"
+              //overflowX: "auto",
+              height: "100%",
+              width: "100%"
             }}
             ref={this.props.fwd}
             src={src}
@@ -223,7 +311,6 @@ export default React.forwardRef((props, ref) => <Cable fwd={ref} {...props} />);
           //continuee.click();
           //}
           //continuee.parentNode.removeChild(continuee);
-
           return;
         }
         //if (!between && continuee) return continuee.remove();
@@ -269,3 +356,40 @@ export default React.forwardRef((props, ref) => <Cable fwd={ref} {...props} />);
         }
       }, timeou);
  */
+
+/**
+  * const Render = () => {
+  useEffect(() => {
+    const handler = event => {
+      const data = JSON.parse(event.data)
+      console.log("Hello World?", data)
+    }
+    window.addEventListener("message", handler)
+    // clean up
+    return () => window.removeEventListener("message", handler)
+  }, []) // empty array => run only once
+  return (
+    <div>
+      <iframe
+        srcDoc={`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            window.top.postMessage(
+              JSON.stringify({
+                error: false,
+                message: "Hello World"
+              }),
+              '*'
+            );
+          </head>
+          <body>
+            <h1>Content inside an iframe, who knew...</h1>
+          </body>
+        </html>
+      `}
+      />
+    </div>
+  )
+}
+  */
